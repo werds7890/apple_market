005449e6ee55a6a0547d02d030c006eb7a809baf
werds7890 <hihyungul@naver.com>
2023-09-01 10:07:24 +0900

    현재 상품삭제 이외 모두 완료

---
+++ app/src/main/java/com/example/apple_market/DetailActivity.kt
@@ -0,0 +1,62 @@
+package com.example.apple_market
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+import com.example.apple_market.databinding.ActivityDetailBinding
+import com.google.android.material.snackbar.Snackbar
+import java.text.DecimalFormat
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val itemList= if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {  //api 33이상 안드로이드 13이상
+            intent.getParcelableExtra("item",Item::class.java)
+        } else {
+            intent.getParcelableExtra("item")
+        }
+        var like=itemList?.isLike
+        heartView(like,binding)
+        binding.like.setOnClickListener {
+            like=!like!!
+            if(like==LikeResponse.YES){
+                binding.like.setImageResource(R.drawable.full_heart)
+            }else {
+                binding.like.setImageResource(R.drawable.heart)
+            }
+        }
+        if(itemList!=null){
+            binding.goodsData.text=itemList.goodsData
+            binding.detailImg.setImageResource(itemList.imgData)
+            binding.sellerData.text=itemList.sellerData
+            binding.addressData.text=itemList.addressData
+            binding.introduceData.text=itemList.introduceData
+            val dec= DecimalFormat("#,###")
+            val convert=dec.format(itemList.priceData)
+            binding.priceData.text=convert+"원"
+        }
+        binding.previous.setOnClickListener {
+            var positionData=intent.getIntExtra("itemPosition",0)
+            intent.putExtra("likeResult",like)
+            intent.putExtra("position",positionData)
+            setResult(RESULT_OK,intent)
+            finish()
+        }
+    }
+
+    fun heartView(like:Boolean?,binding:ActivityDetailBinding){
+        if(like==LikeResponse.YES){
+            binding.like.setImageResource(R.drawable.full_heart)
+        }else {
+            binding.like.setImageResource(R.drawable.heart)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/apple_market/MyAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.apple_market
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apple_market.databinding.MarketItemBinding
+import java.text.DecimalFormat
+
+class MyAdapter(private val items : MutableList<Item>) : RecyclerView.Adapter<MyAdapter.Holder>() {
+
+    interface ItemClick {
+        fun onClick(view: View, position : Int) //데이터 받고 메인액티비티에서 재정의
+    }
+
+    var itemClick : ItemClick?=null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.Holder {
+        val binding=MarketItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyAdapter.Holder, position: Int) {
+        holder.itemView.setOnClickListener { //클릭이벤트추가부분, 여기서 it은 클릭 이벤트가 발생한 뷰를 말함
+            itemClick?.onClick(it,position)
+        }
+        holder.img.setImageResource(items[position].imgData)
+        holder.address.text=items[position].addressData
+        holder.goodsName.text=items[position].goodsData
+        val dec=DecimalFormat("#,###")
+        val convert=dec.format(items[position].priceData)
+        holder.price.text=convert.toString()+"원"
+        holder.like.text=items[position].likeData.toString()
+        holder.comment.text=items[position].commentData.toString()
+
+        if(items[position].isLike){
+            holder.heart.setImageResource(R.drawable.full_heart)
+        }else {
+            holder.heart.setImageResource(R.drawable.heart)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    //viewHolder생성
+    inner class Holder(private val binding : MarketItemBinding) : RecyclerView.ViewHolder(binding.root){
+        val img=binding.imageView
+        val goodsName=binding.goodsName
+        val address=binding.address
+        val price=binding.price
+        val like=binding.like
+        val comment=binding.comment
+
+        val heart=binding.heart
+    }
+
+}
